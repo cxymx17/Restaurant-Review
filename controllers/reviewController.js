6693e832c483045992f8746d7d9fdbf3b98549c1
cxymx17 <90310943+cxymx17@users.noreply.github.com>
2023-12-04 17:02:01 +0800

    Add files via upload

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,36 +1,72 @@
+const mongoose = require('mongoose'); // Import mongoose library
 const Review = require('../models/reviewModel');
 const User = require('../models/userModel');
+const Category = require('../models/categoriesModel');
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Specify the destination folder for uploaded files
 
 const reviewController = {
-  submitReview: async (req, res) => {
+  submitReview: [
+  upload.single('image'),
+  async (req, res) => {
     try {
+      if (!req.session.user || !req.session.user.username) {
+        return res.redirect('/index.html');
+      }
 
-    
-        if (!req.session.user || !req.session.user.username) {
-            // Redirect guests to index.html or handle as needed
-            return res.redirect('/index.html');
-          }
-
-      // Create a new review instance with the request body
+      // Create a new review instance with the request body, including categoryID
       let newReview = new Review({
-        username: req.session.user.username, // Use the username from the session
+        username: req.session.user.username,
         title: req.body.title,
         body: req.body.body,
         recommended: req.body.recommendation === 'recommend',
-        image: req.body.image,
-      });
+        image: req.file ? req.file.path : '', // Save the file path in the database
+        categoryID: req.body.categoryID, // Add categoryID property
+        avatar: req.session.user.avatar ? req.session.user.avatar : 'uploads',
+      });      
 
       // Save the new review to the database
       await newReview.save();
       console.log('Review added successfully');
 
-      // Redirect to a specific page after review submission (e.g., the homepage)
-      res.redirect('/restau1');
+      // Redirect back to the referring page
+      res.redirect('back');
+
     } catch (error) {
       console.error('Error adding review:', error);
       res.status(500).send('Internal Server Error');
     }
   },
+],
+
+
+  // submitReview: [
+  //   upload.single('image'), // Use multer middleware to handle file upload
+  //   async (req, res) => {
+  //     try {
+  //       if (!req.session.user || !req.session.user.username) {
+  //         return res.redirect('/index.html');
+  //       }
+
+  //       let newReview = new Review({
+  //         username: req.session.user.username,
+  //         title: req.body.title,
+  //         body: req.body.body,
+  //         recommended: req.body.recommendation === 'recommend',
+  //         image: req.file ? req.file.path : '', // Save the file path in the database
+  //       });
+
+  //       await newReview.save();
+  //       console.log('Review added successfully');
+
+  //       res.redirect('/restau1');
+  //     } catch (error) {
+  //       console.error('Error adding review:', error);
+  //       res.status(500).send('Internal Server Error');
+  //     }
+  //   },
+  // ],
 
   getRestaurants: async (req, res) => {
     try {
@@ -47,8 +83,127 @@ const reviewController = {
         res.status(500).send('Internal Server Error');
     }
   },
+
+  deleteReview: async (req, res) => {
+    const reviewId = req.params.reviewId;
+
+    try {
+      if (!req.session.user || !req.session.user.username) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const reviewToDelete = await Review.findById(reviewId);
+
+      if (!reviewToDelete) {
+        return res.status(404).json({ error: 'Review not found' });
+      }
+
+      // Check if the logged-in user is the owner of the review
+      if (reviewToDelete.username !== req.session.user.username) {
+        return res.status(403).json({ error: 'Forbidden: Cannot delete review' });
+      }
+
+      // Delete the review
+      await Review.findByIdAndDelete(reviewId);
+
+      res.json({ message: 'Review deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting review:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  
+
+
+
+
+
+  editReview: async (req, res) => {
+    try {
+  
+      //console.log('Inside editReview function');
+      const reviewId = req.params.reviewId;
+  
+      // Ensure reviewId is a valid ObjectId before querying the database
+      if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+        return res.status(400).json({ error: 'Invalid reviewId' });
+      }
+  
+      // Fetch the review from the database based on the reviewId
+      const review = await Review.findById(reviewId);
+  
+      if (review) {
+        // Render the 'editReview.ejs' file and pass the review data
+        res.render('editReview', { review });
+      } else {
+        res.status(404).json({ error: 'Review not found' });
+      }
+    } catch (error) {
+      console.error('Error rendering editReview page:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+  
+  
+  updateReview: async (req, res) => {
+    try {
+      const reviewId = req.params.reviewId;
+  
+      // Fetch the review from the database based on the reviewId
+      const review = await Review.findById(reviewId);
+  
+      if (review) {
+        // Update the review data based on the form submission
+        review.title = req.body.title;
+        review.body = req.body.body;
+        review.recommended = req.body.recommendation === 'true';
+        
+        // Check if a new file is uploaded
+        if (req.file) {
+          review.image = req.file.path;
+        }
+  
+        // Save the updated review to the database
+        await review.save();
+  
+        res.redirect('/profile');
+      } else {
+        res.status(404).json({ error: 'Review not found' });
+      }
+    } catch (error) {
+      console.error('Error updating review:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+
+
+  markAsHelpful: async (req, res) => {
+    const reviewId = req.params.reviewId;
+    try {
+        const review = await Review.findByIdAndUpdate(reviewId, { $inc: { helpful: 1 } }, { new: true });
+        res.json(review);
+    } catch (error) {
+        console.error('Error marking as helpful:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+},
+
+markAsUnhelpful: async (req, res) => {
+    const reviewId = req.params.reviewId;
+    try {
+        const review = await Review.findByIdAndUpdate(reviewId, { $inc: { unhelpful: 1 } }, { new: true });
+        res.json(review);
+    } catch (error) {
+        console.error('Error marking as unhelpful:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+},
   
 
 };
+
+
 
 module.exports = reviewController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,35 @@
 const User = require('../models/userModel');
 const Review = require('../models/reviewModel');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+
+
 
 const userController = {
   viewProfile: async (req, res) => {
     try {
-
-    
       if (!req.session.user || !req.session.user.username) {
         // Redirect guests to index.html or handle as needed
         return res.redirect('/index.html');
       }
-
-      const { username } = req.session.user; 
-
+  
+      const { username } = req.session.user;
+  
       // Retrieve user information and their reviews from the database
       const user = await User.findOne({ username });
       const userReviews = await Review.find({ username });
-
-      
+  
       const reviews = await Review.find();
-
+  
       if (user) {
-        // Render the profile page with user information and all reviews
-        res.render('profile', { user, userReviews, reviews });
+        // Replace backslashes with forward slashes in the image path
+        if (user.avatar) {
+          user.avatar = user.avatar.replace(/\\/g, '/');
+        }
+  
+        // Pass the 'avatar' field along with other user information to the EJS template
+        res.render('profile', { user, userReviews, reviews, avatar: user.avatar }); // Pass 'avatar' field
       } else {
         res.status(404).json({ error: 'User not found' });
       }
@@ -33,94 +40,122 @@ const userController = {
   },
 
   
-  
   login: async (req, res) => {
     try {
       const { username, password } = req.body;
-
+  
       // Validate username and password 
       if (!username || !password) {
         return res.status(400).json({ error: 'Username and password are required.' });
       }
-
+  
       // Check if the user exists in the database
-      const user = await User.findOne({ username, password });
-
+      const user = await User.findOne({ username });
+  
       if (user) {
-        // Successful login, set the user information in the session
-        req.session.user = user;
-        console.log('Login successful for user:', user.username);
-        return res.status(200).json({ message: 'Login successful' });
+        // Compare the hashed input password with the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+  
+        if (passwordMatch) {
+          // Successful login, set the user information in the session
+          req.session.user = user;
+          console.log('Login successful for user:', user.username);
+          return res.status(200).json({ message: 'Login successful' });
+        } else {
+          console.log('Invalid username or password:', username);
+          return res.status(401).json({ error: 'Invalid username or password.' });
+        }
       } else {
-        console.log('Invalid username or password:', username);
-        return res.status(401).json({ error: 'Invalid username or password.' });
+        console.log('User not found:', username);
+        return res.status(401).json({ error: 'User not found.' });
       }
     } catch (error) {
       console.error('Error during login:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
-
+  
   register: async (req, res) => {
     try {
       const { fullname, username, password } = req.body;
-
-      // Validate required fields for registration
+  
       if (!fullname || !username || !password) {
         return res.status(400).send('All fields (except Description) are required for registration.');
       }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const avatarPath = req.file ? req.file.path : '/uploads';
+      console.log('Avatar Path:', avatarPath);
+      
+      // Add the check for user.avatar here
+      if (!avatarPath) {
+        avatarPath = ''; // Set to an empty string if it's not available
+      }
 
-      // Create a new user instance with the request body
       let newUser = new User({
         fullname,
         username,
-        password,
+        password: hashedPassword,
         description: req.body.description,
+        avatar: avatarPath,
       });
-
+  
       // Save the new user to the database
       await newUser.save();
       console.log('User added successfully');
-
-      // Redirect to the home page or any other desired page
+  
+      // Update the avatar in associated reviews
+      const reviews = await Review.find({ username });
+      for (const review of reviews) {
+        // Only update the review's avatar if the user's avatar path is not empty
+        if (avatarPath !== '/uploads') {
+          review.avatar = avatarPath;
+          await review.save();
+        }
+      }
+  
       res.redirect('/');
     } catch (error) {
       console.error('Error adding user:', error);
       res.status(500).send('Internal Server Error');
     }
   },
+  
+  
 
-  updateProfile: async (req, res) => {
-    try {
-      const { username } = req.session.user;
 
-      const user = await User.findOne({ username });
-      const reviews = await Review.find({ username });
+updateProfile: async (req, res) => {
+  try {
+    const { username } = req.session.user;
 
-      user.fullname = req.body.fullname;
-      user.username = req.body.username;
-      user.password = req.body.password;
-      user.description = req.body.description;
+    const user = await User.findOne({ username });
+    const reviews = await Review.find({ username });
 
-      if (req.file) {
-        user.profilePicture = req.file.path;
-      }
+    user.fullname = req.body.fullname;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.description = req.body.description;
 
-      for (const review of reviews) {
-        review.username = req.body.username;
-        await review.save();
-      }
-
-      await user.save();
-
-      req.session.user = user;
-
-      res.redirect('/profile');
-    } catch (error) {
-      console.error('Error updating profile:', error);
-      res.status(500).send('Internal Server Error');
+    if (req.file) {
+      user.avatar = req.file.path;
     }
-  },
+
+    for (const review of reviews) {
+      review.username = req.body.username;
+      await review.save();
+    }
+
+    await user.save();
+
+    req.session.user = user;
+
+    res.redirect('/profile');
+  } catch (error) {
+    console.error('Error updating profile:', error);
+    res.status(500).send('Internal Server Error');
+  }
+},
 
 };
 
